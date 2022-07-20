f76a4a875e7e210f36f2b4089f590ddf0242d079
thng292 <62800118+thng292@users.noreply.github.com>
2022-07-20 16:30:48 +0700

    Almost Done
    Still have to fix the navbar and add dark mode

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// document.getElementsByName('body').onscroll = function () {
+//     a();
+// }
+
+// function a() {
+//     document.getElementsByClassName('navbar-container').style.backgroundColor = 'white';
+// }
