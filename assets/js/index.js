c308cbf4bfffb932de432365c280e977361c2c66
thng292 <62800118+thng292@users.noreply.github.com>
2022-07-21 13:40:02 +0700

    Maybe Final
    Added dark mode

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,47 @@
-// document.getElementsByName('body').onscroll = function () {
-//     a();
-// }
+navbar = document.getElementById('test');
+blockselect = document.getElementsByClassName('selection');
+// console.log(window.scrollY);
 
-// function a() {
-//     document.getElementsByClassName('navbar-container').style.backgroundColor = 'white';
-// }
+function changeNavbarColor() {
+    if (window.scrollY !== 0) {
+        navbar.style.display = 'block';
+        if (darkMode) {
+            for (i = 0; i < 4; i++) {
+                blockselect[i].style.backgroundColor = '#222';
+                blockselect[i].style.boxShadow = '0 0 5px rgba(128,128,128,0.5)';
+            }
+        } else {
+            for (i = 0; i < 4; i++) {
+                blockselect[i].style.boxShadow = '0 0 5px rgba(128,128,128,0.5)';
+                blockselect[i].style.backgroundColor = '#fff';
+            }
+        }
+    } else {
+        navbar.style.display = 'none';
+        for (i = 0; i < 4; i++) {
+            blockselect[i].style.boxShadow = '0 0 0 #000';
+            blockselect[i].style.backgroundColor = 'transparent';
+        }
+    }
+}
+
+var darkMode = false;
+lightCss = document.getElementById('light-css');
+darkCss = document.getElementById('dark-css');
+themeBtn = document.getElementById('toDarkMode');
+mainBody = document.getElementsByClassName('main');
+function changeTheme() {
+    console.log(themeBtn.innerHTML);
+    if (!darkMode) {
+        themeBtn.innerHTML = "Dark mode:  On"
+        darkMode = true;
+        darkCss.media = '';
+        lightCss.media = 'none';
+        changeNavbarColor();
+    } else {
+        themeBtn.innerHTML = "Dark mode: Off"
+        darkMode = false;
+        darkCss.media = 'none';
+        lightCss.media = '';
+    }
+}
